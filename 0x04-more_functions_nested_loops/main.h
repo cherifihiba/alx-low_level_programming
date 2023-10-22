018e0e5f51844afa80fafe21669010a946864b03
meowcode101 <cherifi.hiba.2019@gmail.com>
2023-10-22 21:13:48 +0100

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef main_h
-#define main_h
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 int _isupper(int c);
@@ -11,5 +11,5 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void print_number(int n);
+void print_numbers(void);
 #endif
