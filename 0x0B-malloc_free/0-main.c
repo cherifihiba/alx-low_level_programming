5d476a94b9be5ae27ff3388542708d9b753904e7
meowcode101 <cherifi.hiba.2019@gmail.com>
2023-11-09 20:38:09 +0100

    d

--- 0x0B-malloc_free/0-main.c
+++ 0x0B-malloc_free/0-main.c
@@ -40,9 +40,9 @@ int main(void)
     char *buffer;
 
     buffer = create_array(98, 'H');
-    if  (buffer == NULL)
+    if (buffer == NULL)
     {
-        printf("failed to allocate memory\n");
+        printf("Failed to allocate memory\n");
         return (1);
     }
     simple_print_buffer(buffer, 98);
