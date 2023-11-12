52a353e6eecc5921d59c7745336fee6ca946cf19
meowcode101 <cherifi.hiba.2019@gmail.com>
2023-11-12 20:04:34 +0100

    2 main.c

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "3-function_like_macro.h"
+#include "3-function_like_macro.h"
+
+/**
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
