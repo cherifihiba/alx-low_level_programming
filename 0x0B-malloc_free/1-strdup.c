210c17f6bcfdcfe9d1d1979ef75f666e27f025e1
meowcode101 <cherifi.hiba.2019@gmail.com>
2023-11-12 17:06:31 +0100

    0-malloc_checked.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -23,7 +23,7 @@ char *_strdup(char *str)
     aaa = malloc(sizeof(char) * (i + 1));
 
     if (aaa == NULL)
-        return (NULL);
+	return (NULL);
 
     for (r = 0; str[r]; r++)
         aaa[r] = str[r];
