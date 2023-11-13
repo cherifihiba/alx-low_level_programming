aa095eb448200add353bce9a1dc29816c6dc1625
meowcode101 <cherifi.hiba.2019@gmail.com>
2023-11-13 01:05:45 +0100

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+
 /**
  * int_index - return index place if comparison = true, else -1
  * @array: array
@@ -10,11 +11,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 	if (array == NULL || size <= 0 || cmp == NULL)
-		return (-1);
+	return (-1);
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
-			return (i);
+		return (i);
 	}
 	return (-1);
 }
