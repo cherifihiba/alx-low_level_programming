afda30074e1433a491bc871c1f5010330170943c
meowcode101 <cherifi.hiba.2019@gmail.com>
2023-11-23 00:40:54 +0100

    done

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -6,16 +6,15 @@
  *
  * Return: number of nodes
  */
-
 size_t listint_len(const listint_t *h)
 {
 	size_t num = 0;
 
 	while (h)
- {
-	num++;
-	h = h->next;
- }
+	{
+		num++;
+		h = h->next;
+	}
 
 	return (num);
 }
