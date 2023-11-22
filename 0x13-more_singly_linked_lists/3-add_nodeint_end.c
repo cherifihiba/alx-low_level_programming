6c0b8149f7d1dd3e958e0e9701327e6906d1e5b3
meowcode101 <cherifi.hiba.2019@gmail.com>
2023-11-23 00:43:28 +0100

    done

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -7,7 +7,6 @@
  *
  * Return: pointer to the new node, or NULL if it fails
  */
-
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new;
@@ -21,10 +20,10 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	new->next = NULL;
 
 	if (*head == NULL)
- {
-	*head = new;
- 	return (new);
- }
+	{
+		*head = new;
+ 		return (new);
+	}
 
  	while (temp->next)
  	temp = temp->next;
