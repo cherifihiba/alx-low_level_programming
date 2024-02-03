da78ca9e9115ea98819db5a60f085c215db8b566
meowcode101 <cherifi.hiba.2019@gmail.com>
2024-02-03 17:32:08 +0100

    Done

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,67 +1,30 @@
 #include "lists.h"
 
 /**
- * print_dlistint - Prints all the elements of a dlistint_t list.
- * @h: The head of the dlistint_t list.
+ * print_dlistint - prints all the elements of a
+ * dlistint_t list
  *
- * Return: The number of nodes in the list.
+ * @h: head of the list
+ * Return: the number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    /*Variable to store the number of nodes in the list.*/
-    size_t nodes = 0;
+	int count;
 
+	count = 0;
 
-    while (h)
-    {
-        /*Increment the node count.*/
-        nodes++;
+	if (h == NULL)
+		return (count);
 
+	while (h->prev != NULL)
+		h = h->prev;
 
-        /*Print the value of the current node.*/
-        printf("%d\n", h->n);
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
 
-
-        /*Move to the next node.*/
-        h = h->next;
-    }
-
-
-    /*Return the total number of nodes in the list.*/
-    return (nodes);
-}
-
-
-1-dlistint_len.c
-
-#include "lists.h"
-
-
-/**
- * dlistint_len - Counts the number of elements in a linked dlistint_t list.
- * @h: The head of the dlistint_t list.
- *
- * Return: The number of elements in the dlistint_t list.
- */
-size_t dlistint_len(const dlistint_t *h)
-{
-    /*Variable to store the number of elements in the list.*/
-    size_t nodes = 0;
-
-
-    while (h)
-    {
-        /*Increment the element count.*/
-        nodes++;
-
-
-
-
-        /*Move to the next element.*/
-        h = h->next;
-    }
-
-
-    /*Return the total number of elements in the list.*/
-    return (nodes);
+	return (count);
 }
