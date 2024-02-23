6a4aad7e787cecab387c014182fd801cb1f180ab
Hiba Cherifi <cherifi.hiba.2019@gmail.com>
2024-02-23 08:43:15 -0800

    Done

---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Prints a string to stdout followed by a newline
+ * @str: The string to be printed
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		putchar(*str);
+		str++;
+	}
+	putchar('\n');
+}
